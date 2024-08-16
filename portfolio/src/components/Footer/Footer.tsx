4881dde4a8d01b296867a60a100d21e0cd5f4cdf
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-16 13:04:10 +0100

    push

---
+++ portfolio/src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/rjshkhr/cleanfolio"
+      className="link footer__link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Muyiwa Obaremi
+    </a>
+  </footer>
+);
+
+export default Footer;
---
+++ portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { header } from '../../portfolio';
+import Navbar from '../Navbar/Navbar';
+import './Header.css';
+
+interface HeaderProps {
+  homepage: string | null;
+  title: string;
+}
+
+const Header = () => {
+  const { homepage, title }: HeaderProps = header;
+
+  return (
+    <header className="header center">
+      <h3>
+        {homepage ? (
+          <a href={homepage} className="link">
+            {title}
+          </a>
+        ) : (
+          title
+        )}
+      </h3>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
