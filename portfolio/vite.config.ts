316f9e67523797d97377f7b799e5d71d28aabe79
Muyi2905 <obaremimuyiwa@gmail.com>
2024-10-21 09:31:38 +0100

    update

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './public',
+  root: './public',  // Optional, if your project structure requires a custom root
   plugins: [react()],
   server: {
     port: 8080,
     open: true,
+  },
+  build: {
+    outDir: '../dist',  // Set the output directory to 'dist'
   },
 });
